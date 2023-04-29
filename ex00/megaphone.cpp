66357fb46e896080f89d72840ab523530be882b1
jgo-42 <jingmuio@gmail.com>
2023-04-29 11:38:13 +0900

    :recycle: Refactor: type casting remove

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jgo <jgo@student.42seoul.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:21:18 by jgo               #+#    #+#             */
-/*   Updated: 2023/04/21 11:15:37 by jgo              ###   ########.fr       */
+/*   Updated: 2023/04/29 11:34:30 by jgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 #define MEGAPHONE_NOISE "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 int main(int ac, char** av) {
-    if (ac == 1)
-        std::cout << MEGAPHONE_NOISE;
-    else {
-        for (int i = 1; av[i]; i++)
-            for (int j = 0; av[i][j]; j++)
-                std::cout << (char)std::toupper(av[i][j]);
-    }
-    std::cout << std::endl;
-    return (0);
+	if (ac == 1)
+		std::cout << MEGAPHONE_NOISE;
+	else {
+		for (int i = 1; av[i]; i++) {
+			for (int j = 0; av[i][j]; j++)
+				av[i][j] = std::toupper(av[i][j]);
+			std::cout << av[i];
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
